5d26cadc6ffc630a012fc19be9126b71bcee59b4
ayman <aymantheremkus11@gmail.com>
2025-10-05 23:00:15 +0300

    Add Clearance Level

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_05_192301) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_05_193002) do
   create_table "sessions", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "ip_address"
@@ -25,6 +25,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_05_192301) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "clearance_level", default: 1
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
   end
 
