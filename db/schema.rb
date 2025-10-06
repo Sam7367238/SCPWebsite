285b133bd619c2cfefffe64c4819fba659d1fb88
ayman <aymantheremkus11@gmail.com>
2025-10-06 22:35:06 +0300

    Forgot This; SCP Scaffold Resource And Schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_05_193002) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_06_191816) do
+  create_table "scps", force: :cascade do |t|
+    t.integer "item_number"
+    t.string "classification"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "sessions", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "ip_address"
